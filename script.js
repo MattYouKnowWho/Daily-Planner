dff3fe2264a8c443eec2480e0931c0d379df8edb
Matt Dunston <matt.cdunston21@gmail.com>
2022-08-30 03:31:38 -0700

    added variables within the new script.js file

---
+++ script.js
@@ -0,0 +1,5 @@
+var grabCurrentDate = moment().format('MMMM Do, YYYY');
+var grabCurrentHour = moment().format('h a');
+var buttons = document.querySelectorAll('.saveBtn')
+var userInput = document.querySelectorAll('.user-input')
+var timeBlock = document.querySelectorAll('.time-block')
